4bc0ffeaf87f816c71219b5aaab1d1cc9d52e384
parask7 <137814144+parask7@users.noreply.github.com>
2024-08-30 22:57:56 +0530

    Add files via upload

---
+++ project 1/main.py
@@ -0,0 +1,36 @@
+import random
+
+computer = random.choice([-1, 0, 1])
+youstr = input("Enter your choice\n1:rock\n2:paper\n3:seccior\n")
+youdict = {
+    "rock" : -1,
+    "paper" : 0,
+    "seccior": 1
+}
+reversdict = {
+    -1: "rock",
+    0: "paper",
+    1: "seccior"
+}
+
+you = youdict[youstr]
+
+print(f"your choise {reversdict[you]}\ncomputer choise {reversdict[computer]}")
+
+if(computer == you):
+    print("it's a draw")
+else:
+    if(computer == 1 and you == -1):
+        print("you win")
+    elif(computer == 1 and you == 0):
+        print("you loss")
+    elif(computer == -1 and you == 1):
+        print("you loss")
+    elif(computer == -1 and you == 0):
+        print("you win")
+    elif(computer == 0 and you == -1):
+        print("you loss")
+    elif(computer == 0 and you == 1):
+        print("you win")
+    else:
+        print("somthing went wrong")
